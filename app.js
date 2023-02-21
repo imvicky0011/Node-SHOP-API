cd5cd95ca907004487ad85a24c3439961c3917d8
imvicky0011 <kumarvikku0011@gmail.com>
2023-02-21 13:16:02 +0530

    commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(morgan("dev"));
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 const bodyParser = require("body-parser");
 
 
@@ -40,6 +41,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not Found!");
